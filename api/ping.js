de19fd5fb9d0c8186342ce15ce6107d19fe1ac9e
lightningmango14 <epidermyx1@gmail.com>
2025-09-21 23:36:35 -0300

    Delete api/ping.js

--- api/ping.js
+++
@@ -1,5 +0,0 @@
-// api/ping.js
-module.exports = (_req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).end(JSON.stringify({ ok: true, hello: 'from serverless' }));
-};
